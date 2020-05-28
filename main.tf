41421a23a3ffb773d951596e8561063729c54701
Benjamin Brewer <ben@benbrewer.me>
2020-05-28 17:09:57 -0400

    move things that are wired up to lambda elsewhere

--- main.tf
+++ main.tf
@@ -92,84 +92,6 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
 }
 
 //---------EVENTS---------//
-resource "aws_cloudwatch_event_rule" "guardduty" {
-  name        = "${terraform.workspace}-guardduty"
-  description = "Capture whenever a GuardDuty event is seen"
-
-  event_pattern = <<PATTERN
-{
-  "source": [
-    "aws.guardduty"
-  ]
-}
-PATTERN
-}
-
-resource "aws_cloudwatch_event_target" "guardduty_to_sns" {
-  rule      = "${aws_cloudwatch_event_rule.guardduty.name}"
-  target_id = "GuardDutyToSNS"
-  arn       = "${aws_sns_topic.alert_handler_sns_topic.arn}"
-}
-
-//---------ALARMS---------//
-
-resource "aws_cloudwatch_metric_alarm" "joomla_rds_free_storage_space_low" {
-  count               = "${var.joomla_alarms_enabled}"
-  alarm_name          = "${terraform.workspace} Joomla - RDS Free Storage Space Low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "FreeStorageSpace"
-  namespace           = "AWS/RDS"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "15000000000"
-  alarm_actions       = ["${aws_sns_topic.alert_handler_sns_topic.arn}"]
-
-  dimensions {
-    DBInstanceIdentifier = "${var.joomla_db_id}"
-  }
-
-  treat_missing_data = "breaching"
-}
-
-resource "aws_cloudwatch_metric_alarm" "joomla_rds_high_cpu_util" {
-  count               = "${var.joomla_alarms_enabled}"
-  alarm_name          = "${terraform.workspace} Joomla - RDS High CPU Utilization"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/RDS"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "90"
-  alarm_actions       = ["${aws_sns_topic.alert_handler_sns_topic.arn}"]
-
-  dimensions {
-    DBInstanceIdentifier = "${var.joomla_db_id}"
-  }
-
-  treat_missing_data = "breaching"
-}
-
-resource "aws_cloudwatch_metric_alarm" "watchintor_cota_streaming_consumer_open_connection_failed" {
-  alarm_name          = "${terraform.workspace} Watchinator - Cota Streaming Consumer Open Connection Failed"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "Opened"
-  namespace           = "Socket Connection"
-  period              = "120"
-  statistic           = "Sum"
-  threshold           = "1"
-  alarm_actions       = ["${aws_sns_topic.alert_handler_sns_topic.arn}"]
-
-  dimensions {
-    ApplicationName = "Cota-Streaming-Consumer"
-  }
-
-  treat_missing_data = "breaching"
-}
-
-//-----------------------//
 
 variable "alarms_slack_path" {
   description = "Path to the Slack channel"
@@ -179,12 +101,6 @@ variable "alarms_slack_channel_name" {
   description = "Name of the Slack channel"
 }
 
-variable "joomla_alarms_enabled" {
-  description = "Enables Joomla Cloudwatch alarms. Defaults to true."
-  default     = true
-}
-
-variable "joomla_db_id" {
-  description = "The Joomla Database ID for the monitoring stack to watch (if enabled)"
-  default     = ""
+output "alert_handler_sns_topic_arn" {
+  value = "${aws_sns_topic.alert_handler_sns_topic.arn}"
 }
